530066d0001229596297c9ee1c112bad140616b2
GfTechHub <olaleyeezekiel06@gmail.com>
2025-11-16 15:42:01 +0100

    feat: integrate AOS animations and smooth scroll interactions for enhanced UX

---
+++ js/animation.js
@@ -0,0 +1,13 @@
+AOS.init({
+  duration: 900,
+  once: true,
+});
+
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
